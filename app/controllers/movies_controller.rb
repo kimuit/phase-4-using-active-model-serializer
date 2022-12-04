9604cf6782d98a1a98d0befbcb9a3814a8cc58b3
kimuit117 <kemoikiuita@gmail.com>
2022-12-04 13:18:02 +0300

    done

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -7,13 +7,27 @@ class MoviesController < ApplicationController
   end
 
   def show
-    movie = Movie.find(params[:id])
-    render json: movie
+  movie = Movie.find(params[:id])
+  render json: movie
   end
 
+  def summary
+  movie = Movie.find(params[:id])
+  render json: movie, serializer: MovieSerializer
+end
   private
 
   def render_not_found_response
     render json: { error: "Movie not found" }, status: :not_found
   end
 end
+
+# def show
+#   movie = Movie.find(params[:id])
+#   render json: movie.to_json(only: [:id, :title, :year, :length, :director, :description, :poster_url, :category, :discount, :female_director])
+# end
+
+# def show
+#   post = Post.find(params[:id])
+#   render json: post.to_json(only: [:title, :description, :id], include: [author: { only: [:name]}])
+# end
